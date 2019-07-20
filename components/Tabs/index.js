121630fb958e57084b955b5e754e8b1280d9734d
Mada Gower <gmgower@gmail.com>
2019-07-19 22:24:09 -0500

    Clean up code and whitespace.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,36 +8,34 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-// * Setup axios promise to GET request to address. 
-const topicsEntry = document.querySelector('.topics')
+// * Setup axios promise to GET request to address.
+const topicsEntry = document.querySelector('.topics');
 
-axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
-    .then(ltData => {
-        console.log(`Lambda Times Data`, ltData.data.topics)
-        ltData.data.topics.forEach(topic => {
-            topicsEntry.appendChild(createTab(topic))
-        })
-    })
-    .catch(error => {
-        console.log(`Lambda Times not avaliable`, error)
-    })
-
-
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/topics`)
+  .then(ltData => {
+    console.log(`Lambda Times Data`, ltData.data.topics);
+    ltData.data.topics.forEach(topic => {
+      topicsEntry.appendChild(createTab(topic));
+    });
+  })
+  .catch(error => {
+    console.log(`Lambda Times not avaliable`, error);
+  });
 
 // * Check Dom
 // const tabEntry = document.querySelector('.topics')
 // tabEntry.appendChild(createTab())
 
+function createTab(topicObj) {
+  // * Define elements
+  const tabs = document.createElement('div');
 
-function createTab (topicObj){
-    // * Define elements
-    const tabs = document.createElement('div')
+  // * Setup classes
+  tabs.classList.add('tab');
 
-    // * Setup classes
-    tabs.classList.add('tab')
+  // * Setup text content
+  tabs.textContent = topicObj;
 
-    // * Setup text content
-    tabs.textContent = topicObj
-
-    return tabs
+  return tabs;
 }
